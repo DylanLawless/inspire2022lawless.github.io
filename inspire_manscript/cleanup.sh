e1acd1142092c35d8f5db370276b588a08163b5a
DylanLawless <dylan.lawless@epfl.ch>
2021-10-29 17:08:51 +0200

    cleanup

--- inspire_manscript/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-rm inspire_pre-print_v1.aux
-rm inspire_pre-print_v1.bbl
-rm inspire_pre-print_v1.blg
-rm inspire_pre-print_v1.log
-rm inspire_pre-print_v1.out
-rm inspire_pre-print_v1.synctex.gz
