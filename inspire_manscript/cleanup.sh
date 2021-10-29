f86353ce801847e2a3e6dd0281f9c4a0641c175a
DylanLawless <dylan.lawless@epfl.ch>
2021-10-29 17:04:00 +0200

    hidden dir to ease overleaf

---
+++ inspire_manscript/cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+rm inspire_pre-print_v1.aux
+rm inspire_pre-print_v1.bbl
+rm inspire_pre-print_v1.blg
+rm inspire_pre-print_v1.log
+rm inspire_pre-print_v1.out
+rm inspire_pre-print_v1.synctex.gz
